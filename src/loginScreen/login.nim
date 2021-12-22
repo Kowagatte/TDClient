8d000e81f4297d52fb4098a54d2496492fd38c19
Nick Ryan <nnryanp@gmail.com>
2021-12-22 17:22:43 -0600

    Fixed some of the UI placement in login and create screen aswell as font & button fixes

--- src/loginScreen/login.nim
+++ src/loginScreen/login.nim
@@ -1,7 +1,7 @@
 import ../connection
 import godot
 import godotinternal
-import godotapi/[engine, node, line_edit, button, input, global_constants]
+import godotapi/[engine, node, line_edit, button, input, global_constants, link_button, scene_tree]
 import ../utils/packets
 
 gdobj Login of Node:
@@ -10,12 +10,15 @@ gdobj Login of Node:
     emailEnter: LineEdit
     passwordEnter: LineEdit
     loginButton: Button
+    createAccountButton: LinkButton
 
   method ready*() =
     self.emailEnter = self.getNode(newNodePath("Email/EmailEnter")) as LineEdit
     self.passwordEnter = self.getNode(newNodePath("Password/PasswordEnter")) as LineEdit
     self.loginButton = self.getNode(newNodePath("LoginButton")) as Button
+    self.createAccountButton = self.getNode(newNodePath("CreateAccount/CreateAccountButton")) as LinkButton
     discard self.loginButton.connect("pressed", self, "_send_credentials")
+    discard self.createAccountButton.connect("pressed", self, "_create_account")
 
   method sendCredentials() =
     var loginPacket: ClientPacket_LoginPacket = newClientPacket_LoginPacket()
@@ -36,3 +39,6 @@ gdobj Login of Node:
   method debugLoginCredentials*() =
     print("Email:", self.emailEnter.text)
     print("Password:", self.passwordEnter.text)
+
+  method createAccount*() =
+    discard self.getTree().changeSceneImpl("res://scenes/CreateAccountScreen.tscn")

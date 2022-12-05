79b6caf3e38f8fb4c245b6226c21982588dec3b4
Nicholas Ryan <nnr635@usask.ca>
2022-12-04 23:28:48 -0600

    Added login screen

---
+++ scripts/loginscreen.gd
@@ -0,0 +1,20 @@
+extends Control
+
+onready var emailEnter = get_node("Panel/Email/EmailEnter") as LineEdit
+onready var passwordEnter = get_node("Panel/Password/PasswordEnter") as LineEdit
+onready var loginButton = get_node("Panel/LoginButton") as Button
+onready var createAccountButton = get_node("Panel/CreateAccount/CreateAccountButton") as LinkButton
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var _p = loginButton.connect("pressed", self, "login")
+	var _c = createAccountButton.connect("pressed", self, "createAccount")
+
+func login():
+	if emailEnter != "":
+		if passwordEnter != "":
+			pass
+
+func createAccount():
+	var _c = get_tree().change_scene("res://screens/CreateAccountScreen.tscn")

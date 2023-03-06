dccd5c059d70590558cdf52b28c8039b40d8b220
Nicholas Ryan <nnr635@usask.ca>
2023-03-06 16:25:27 -0600

    Fixed screens to match resolution

--- scripts/connectingscreen.gd
+++ scripts/connectingscreen.gd
@@ -1,7 +1,7 @@
 extends Control
 
 
-@onready var sprite = get_node("Sprite2D")
+@onready var sprite = get_node("Panel/Label/Sprite2D")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
